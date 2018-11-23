efac25a1b6f75df4c51539ff9fc418c2995c936f
William George <wrgeorge1983@gmail.com>
2018-11-22 21:32:37 -0500

    migrate to pipenv vs. requirements.txt

---
+++ rp_static/rp_static/__main__.py
@@ -0,0 +1,4 @@
+from .rp_static import main
+
+if __name__ == '__main__':
+    main()
---
+++ rp_static/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='rp_static',
+    packages=['rp_static'],
+    include_package_data=True,
+    install_requires=[
+        'requests'
+    ],
+    entry_points={
+        'console_scripts': [
+            'rp_static = rp_static.__main__:main'
+        ]
+    }
+)
