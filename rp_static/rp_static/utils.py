1f85abf2eca904622dfaf72a9e543aed4b07cfcc
William George <wrgeorge1983@gmail.com>
2018-12-01 05:22:51 -0500

    add utils.get_configs_by_hostname()

--- rp_static/rp_static/utils.py
+++ rp_static/rp_static/utils.py
@@ -1,10 +1,8 @@
 import yaml
 import logging
+import os
 
 log = logging.getLogger(__name__)
-
-
-
 
 
 def get_configs(config_file, topology_file):
@@ -14,6 +12,24 @@ def get_configs(config_file, topology_file):
         config = yaml.load(config_file)
     else:
         config = {}
+    log.info(f'Using topology file: {topology_file.name}')
+    topology = yaml.load(topology_file)
+    log.debug(f'Got config: \n{yaml.dump(config)}')
+    log.debug(f'Got topology: \n{yaml.dump(topology)}')
+    return {
+        'router_config': config,
+        'topology': topology
+    }
+
+
+def get_configs_by_hostname(config_file_path, topology_file, hostname):
+    log.debug(f'entered get_configs_by_hostname()')
+    config_filename = f'{hostname}.config.yml'
+    full_config_filename = os.path.join((config_file_path, config_filename))
+    log.info(f'Using config file: {config_file_path}')
+    with open(full_config_filename) as infil:
+        config = yaml.load(infil)
+
     log.info(f'Using topology file: {topology_file.name}')
     topology = yaml.load(topology_file)
     log.debug(f'Got config: \n{yaml.dump(config)}')
